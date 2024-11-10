9393840df117e38c22de03cc8344df07a35bf53f
Werthen Castro <wcastro.santos10@gmail.com>
2024-11-10 15:56:34 -0300

    removendo arquivos

--- lib/util/endpoints.dart
+++
@@ -1,9 +0,0 @@
-class PodcastsEndpoints {
-  static const dnsListenNote = 'https://listen-api.listennotes.com/api/v2/';
-
-  static String getBestPodcasts(int page) =>
-      '${dnsListenNote}best_podcasts?page=$page&region=br&language=Portuguese';
-
-  static String getPodcastForId(String id) =>
-      '${dnsListenNote}podcasts/$id?sort=recent_first';
-}
--- lib/util/path_images.dart
+++
@@ -1,4 +0,0 @@
-const pathLogoLogin = 'assets/logo_login.png';
-const pathAppleIcon = 'assets/apple_icon.png';
-const pathFacebookIcon = 'assets/facebook_icon.png';
-const pathGoogleIcon = 'assets/google_icon.png';
