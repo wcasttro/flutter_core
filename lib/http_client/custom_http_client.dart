b69650dde7033a8f8cbdcf080e816ff40325b47a
Werthen Castro <wcastro.santos10@gmail.com>
2024-11-11 09:32:56 -0300

    adicionando metodo network

--- lib/http_client/custom_http_client.dart
+++ lib/http_client/custom_http_client.dart
@@ -1,6 +1,21 @@
 import 'package:core/http_client/custom_http_response.dart';
 
 abstract class CustomHttpClient {
-  Future<CustomHttpResponse> get(String url, {Map<String, dynamic>? heards});
-  Future<CustomHttpResponse> post(String url, {Map? body});
+  Future<CustomHttpResponse> get(
+    String url, {
+    Map<String, dynamic>? heards,
+    Map<String, dynamic>? queryParameters,
+  });
+
+  Future<CustomHttpResponse> post(
+    String url, {
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? queryParameters,
+  });
+
+  Future<CustomHttpResponse> put(
+    String url, {
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? queryParameters,
+  });
 }
--- lib/http_client/custom_http_client_impl.dart
+++ lib/http_client/custom_http_client_impl.dart
@@ -2,11 +2,10 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:core/erros/error_messages.dart';
-import 'package:core/http_client/interceptor_http.dart';
-import 'package:dio/dio.dart';
-
 import 'package:core/http_client/custom_http_client.dart';
 import 'package:core/http_client/custom_http_response.dart';
+import 'package:core/http_client/interceptor_http.dart';
+import 'package:dio/dio.dart';
 
 class CustomHttpClientImpl implements CustomHttpClient {
   final Dio _dio = Dio();
@@ -21,11 +20,12 @@ class CustomHttpClientImpl implements CustomHttpClient {
   Future<CustomHttpResponse> get(
     String url, {
     Map<String, dynamic>? heards,
+    Map<String, dynamic>? queryParameters,
   }) async {
     Response response;
 
     try {
-      response = await _dio.get(url);
+      response = await _dio.get(url, queryParameters: queryParameters);
     } on DioException catch (error) {
       return CustomHttpResponse(
         statusCode: error.response?.statusCode,
@@ -49,11 +49,56 @@ class CustomHttpClientImpl implements CustomHttpClient {
   }
 
   @override
-  Future<CustomHttpResponse> post(String url, {Map? body}) async {
+  Future<CustomHttpResponse> post(
+    String url, {
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? queryParameters,
+  }) async {
     Response response;
 
     try {
-      response = await _dio.post(url, data: body);
+      response = await _dio.post(
+        url,
+        data: body,
+        queryParameters: queryParameters,
+      );
+    } on DioException catch (error) {
+      return CustomHttpResponse(
+        statusCode: error.response?.statusCode,
+        statusMessage: error.response?.statusMessage,
+      );
+    } on TimeoutException catch (_) {
+      return CustomHttpResponse(
+        statusMessage: ErrorMessages.errorTimeout,
+        timeout: true,
+      );
+    } on SocketException catch (exception) {
+      return CustomHttpResponse(
+        statusMessage: exception.message,
+      );
+    }
+
+    return CustomHttpResponse(
+      data: response.data,
+      statusCode: response.statusCode,
+      header: response.headers.map,
+    );
+  }
+
+  @override
+  Future<CustomHttpResponse> put(
+    String url, {
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    Response response;
+
+    try {
+      response = await _dio.put(
+        url,
+        data: body,
+        queryParameters: queryParameters,
+      );
     } on DioException catch (error) {
       return CustomHttpResponse(
         statusCode: error.response?.statusCode,
